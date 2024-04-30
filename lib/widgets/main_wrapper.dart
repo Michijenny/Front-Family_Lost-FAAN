a26d5b110f83bcb4ac487712b40e08fa9cebd373
Michijenny <enriquezjenny29@gmail.com>
2024-04-30 07:33:35 -0500

    .
    uwu

--- lib/widgets/main_wrapper.dart
+++ lib/widgets/main_wrapper.dart
@@ -7,6 +7,7 @@ import 'package:familylost_faan/utilities/texts/app_strings.dart';
 import 'package:familylost_faan/utilities/AssetManager/asset_manager.dart';
 import 'package:familylost_faan/pages/pages.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -44,7 +45,7 @@ class _MainWrapperState extends State<MainWrapper> {
     HomePage(),
     FavoritePage(),
     HomePage(),
-    HomePage(),
+    ProfilePage()
   ];
 
   void onPageChanged(int page) {
